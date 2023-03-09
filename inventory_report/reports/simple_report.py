9cbd91f0285a1b01d954ada1565fe5cb19b6b8b0
Bruno Krauthamer <brunokrauthamer@gmail.com>
2023-03-08 23:45:36 -0300

    req02

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,37 @@
+from datetime import datetime
+
 class SimpleReport:
-    
+    @classmethod
+    def generate(cls, list):
+        current_date = datetime.today().strftime('%Y-%m-%d')
+        fabrication_dates = []
+        expiration_dates = []
+        companies = []
+        for item in list:
+            fabrication_dates.append(item['data_de_fabricacao'])
+            companies.append(item['nome_da_empresa'])
+            if item['data_de_validade'] >= current_date:
+                expiration_dates.append(item['data_de_validade'])
+
+
+        fabrication_dates.sort()
+        expiration_dates.sort()
+
+        oldest_fabrication_date = fabrication_dates[0]
+        closest_expiration_date = expiration_dates[0]
+        more_products_company = ''
+        number_of_products = 0
+
+        print(f'')
+
+        for company in companies:
+            count = companies.count(company)
+            if count > number_of_products:
+                number_of_products = count
+                more_products_company = company
+        
+        return f"""Data de fabricação mais antiga: {oldest_fabrication_date}
+Data de validade mais próxima: {closest_expiration_date}
+Empresa com mais produtos: {more_products_company}"""
+
+
--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -9,7 +9,13 @@ def test_cria_produto():
     data_de_validade = 'data_de_validade'
     numero_de_serie = 1
     instrucoes_de_armazenamento = 'instrucoes_de_armazenamento'
-    product = Product(id, nome_do_produto, nome_da_empresa, data_de_fabricacao, data_de_validade, numero_de_serie, instrucoes_de_armazenamento)
+    product = Product(id,
+    nome_do_produto,
+    nome_da_empresa,
+    data_de_fabricacao,
+    data_de_validade,
+    numero_de_serie,
+    instrucoes_de_armazenamento)
     print(product)
     assert product.id == 1
     assert product.nome_do_produto == nome_do_produto
@@ -18,4 +24,3 @@ def test_cria_produto():
     assert product.data_de_validade == data_de_validade
     assert product.numero_de_serie == 1
     assert product.instrucoes_de_armazenamento == instrucoes_de_armazenamento
-
