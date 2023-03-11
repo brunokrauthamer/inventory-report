c0681e21db8163e0ed5ee66990b56133b4bd1560
Bruno Krauthamer <brunokrauthamer@gmail.com>
2023-03-10 22:05:57 -0300

    req05

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,12 +1,17 @@
 import csv
+import json
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
+
 
 class Inventory:
     @classmethod
     def import_data(cls, path, type):
         with open(path) as data_file:
-            data_list = csv.DictReader(data_file)
+            if path[-1] == 'v':
+                data_list = csv.DictReader(data_file)
+            if path[-1] == 'n':
+                data_list = json.load(data_file)
             data = []
             for d in data_list:
                 data.append(d)
